2b10bc93c85b5055f3c820ea84fc832e7810bc5c
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-11-17 16:29:49 -0500

    Added Relative Filepath Redirect

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,11 @@ setInterval(function() {
 }, 500);
 
 // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
+window.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-    
+        // Cancel the default action, if needed
+        event.preventDefault();
+        window.location = "quest.html";
     }
 });
