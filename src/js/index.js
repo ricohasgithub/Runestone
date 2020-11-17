b5c57df181497b3c0aceb471ad4a3d52268ff25f
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-11-17 16:16:42 -0500

    Updated Font
     - Used Press Start 2P from Google Fonts API instead for quicker response times

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,12 @@ setInterval(function() {
     entertext.style.display = (entertext.style.display == 'none' ? '' : 'none');
 }, 500);
 
-
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+    
+    }
+});
