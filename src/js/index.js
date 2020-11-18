e106d11b07fbf19c147ab7e29964778732325e8a
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-11-18 09:19:48 -0500

    Added Audio Toggling

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 
 let entertext = document.getElementById("game-text");
+let ambienceAudio = document.getElementById("ambience");
+
+let first = true;
+
+function togglePause() {
+    if(ambienceAudio.paused && ambienceAudio.currentTime > 0 && !ambienceAudio.ended) {
+        ambienceAudio.play();
+    } else {
+        ambienceAudio.pause();
+    }
+ }
 
 setInterval(function() {
     entertext.style.display = (entertext.style.display == 'none' ? '' : 'none');
@@ -7,10 +18,17 @@ setInterval(function() {
 
 // Execute a function when the user releases a key on the keyboard
 window.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
+    // Event 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
         // Cancel the default action, if needed
         event.preventDefault();
         window.location = "quest.html";
+    } else if (event.keyCode === 77) {
+        if (first) {
+            ambienceAudio.play();
+            first = !first;
+        } else {
+            togglePause();
+        }
     }
 });
